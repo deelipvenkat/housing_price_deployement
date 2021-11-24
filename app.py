d06ec717030d337c2b3d6560c3ef908621407bce
deelipvenkat <48489548+deelipvenkat@users.noreply.github.com>
2021-11-24 13:26:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import pickle
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
-scaler = load(open('scaler.pkl', 'rb'))
+scaler =pickle.load(open('scaler.pkl', 'rb'))
 @app.route('/')
 def home():
     return render_template('template.html')
